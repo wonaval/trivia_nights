b32e68ef28e945e6b8e0ab6fa72239a58be9296e
Marcolux <mccolucci1@gmail.com>
2021-12-21 21:23:45 -0600

    fixed little bug and added pictures

--- frontend/src/components/Events.js
+++ frontend/src/components/Events.js
@@ -2,7 +2,7 @@ const Events = () => {
     return (
         <div>
             <div>
-                <div clasclassName="homepageComp">
+                <div className="homepageComp">
                     <h3>Events</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Convallis a sed amet et. Iaculis in ut fermentum est. Faucibus quis lectus praesent convallis eu blandit vitae. Ornare turpis habitant mauris, mauris porttitor morbi et.
                     Eu eu etiam vestibulum felis pharetra.
