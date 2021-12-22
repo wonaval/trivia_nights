4c35d0454fe04bfa143d51318b978951f8f25cdc
Marcolux <mccolucci1@gmail.com>
2021-12-22 17:46:50 -0600

    css completed

--- frontend/src/pages/Confirmation.js
+++ frontend/src/pages/Confirmation.js
@@ -1,6 +1,6 @@
 import { useContext } from 'react';
 import { Context } from '../context/Context';
-
+import img from'../images/unsplash_topics.png';
 
 
 const Confirmation = () => {
@@ -12,8 +12,9 @@ const Confirmation = () => {
     const [ password ] = passwordState;
 
     return (
-        <div>
-            <img className="confImage"alt="" />
+        <div className='confirmation'>
+            <img src={img} className="confImage"alt="" />
+            <div className='confirmationRight'>
             <h2>You’ve successfully created your account!</h2>
                 <div className="confInfo">
                     <div>Name: {name} {lastName}</div>
@@ -21,6 +22,7 @@ const Confirmation = () => {
                     <div>Password: {password}</div>
                     <button className="buttons">Browse Events</button>
                 </div>
+            </div>
         </div>
     )
 }
