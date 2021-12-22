42db4bf1b3e70783b2cd313a4f76c29232c8e8ea
wonaval <wonaval@gmail.com>
2021-12-22 15:18:31 -0500

    Dashboard WIP

---
+++ backend/routes/eventRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const eventRoutes = express.Router();
+const eventControllers = require('../controllers/eventControllers');
+
+eventRoutes.get('/', eventControllers.fetchAll)
+
+module.exports = eventRoutes
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,11 +4,6 @@ const userController = require('../controllers/userControllers');
 
 userRoutes.post('/', userController.createUser)
 userRoutes.get('/', userController.fetchAll)
-<<<<<<< HEAD
-userRoutes.get('/:id', userController.fetchUser)
-=======
-// userRoutes.get('/users/:id', userController.fetchUser)
->>>>>>> main
 
 
 module.exports = userRoutes
